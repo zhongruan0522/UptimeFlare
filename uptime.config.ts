db157439df569b49c50edb70593a54c9bc4137f7
阮 <zhongruan0522@gmail.com>
2025-05-08 19:46:42 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,7 +2,7 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "zhongruan's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/zhongruan052', label: 'GitHub' },
