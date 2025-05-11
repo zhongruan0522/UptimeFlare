ffaab03df792f46c148b5b87333af6432087452a
阮 <zhongruan0522@gmail.com>
2025-05-11 15:29:21 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -134,16 +134,16 @@ monitors: [
 const maintenances: MaintenanceConfig[] = [
   {
     // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['foo_monitor', 'bar_monitor'],
+    monitors: ['api_website'],
     // [Optional] default to "Scheduled Maintenance" if not specified
     title: 'Test Maintenance',
     // Description of the maintenance, will be shown at status page
     body: 'This is a test maintenance, server software upgrade',
     // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2025-04-27T00:00:00+08:00',
+    start: '2025-05-11T00:00:00+08:00',
     // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
     // if not specified, the maintenance will be considered as on-going
-    end: '2025-04-30T00:00:00+08:00',
+    end: '2025-05-12T00:00:00+08:00',
     // [Optional] color of the maintenance alert at status page, default to "yellow"
     color: 'blue',
   },
