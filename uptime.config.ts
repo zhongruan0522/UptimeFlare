c682632de95328b0c0c9762ac81e2419bb8651fc
阮 <zhongruan0522@gmail.com>
2025-05-12 14:45:01 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -14,7 +14,7 @@ const pageConfig: PageConfig = {
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
     '主站基础服务': ['my_website', 'chat_website'],
-    'API服务': ['api_website', 'api2_website', 'tkapi_website', 'tkapi2_website'],
+    'API服务': ['api_website', 'api2_website', 'tkapi_website'],
   },
 }
 
@@ -73,16 +73,6 @@ monitors: [
     tooltip: 'TokenAPI监控',
     statusPageLink: 'https://tk.zxiaoruan.cn',
   },
-  // {
-    // id: 'tkapi2_website',
-   //  name: 'tkapi2',
-   //  method: 'GET',
-   //  target: 'https://zgjyzx.zxiaoruan.cn',
-   //  expectedCodes: [200],
-   //  timeout: 10000,
-  //   tooltip: 'zgjyzx-TokenAPI监控',
-  //   statusPageLink: 'https://zgjyzx.zxiaoruan.cn',
- //  },
 ],
     notification: {
     // [Optional] apprise API server URL
@@ -148,19 +138,19 @@ const maintenances: MaintenanceConfig[] = [
     // [Optional] color of the maintenance alert at status page, default to "yellow"
     color: 'blue',
   }, {
-    // [Optional] Monitor IDs to be affected by this maintenance
     monitors: ['chat_website'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
     title: '服务器域名崩溃',
-    // Description of the maintenance, will be shown at status page
     body: '以下服务器正在修复中，如有影响请切换至对应的备用服务器',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
     start: '2025-05-12T11:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
     end: '2025-05-12T15:45:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
     color: 'red',
+  },{
+    monitors: ['my_website','api_website','chat_website','tkapi_website'],
+    title: '服务器数据备份',
+    body: '以下服务正在进行冷备份，部分服务提供备用服务器',
+    start: '2025-05-12T14:40:00+08:00',
+    end: '2025-05-12T15:20:00+08:00',
+    color: 'blue',
   },
 ]
 
